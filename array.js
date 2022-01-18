6ae8d91c11253f4ab269572d6e4b0012964cc44d
Adians07 <97782495+Adians07@users.noreply.github.com>
2022-01-18 07:11:21 -0800

    Add files via upload
    initial commit

---
+++ array.js
@@ -0,0 +1,26 @@
+
+// Array Manipulation Method
+// pop - It removes an element from end of an array
+
+let cars =["Mercedes", "BMW", "Audi"]
+let poppedcar = cars.pop()
+console.log(poppedcar)
+
+
+console.log(cars)
+
+// push - It adds an element at the end of an array
+cars.push('Alto')
+console.log(cars)
+
+
+// Shift - It adds an element at the start of an array
+let Shiftedcar = cars.shift()
+console.log(Shiftedcar)
+
+// unshift - It removes an element at the start of an array
+
+cars.unshift('Alto')
+console.log(cars    )
+
+
