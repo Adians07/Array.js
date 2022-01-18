4794652fce6df3a36b02020cda9eb55c3bb557d2
Adians07 <97782495+Adians07@users.noreply.github.com>
2022-01-18 07:14:49 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+// function definition
+
+//function chotafunction(Array) {
+  //  console.log([10,20,30,40]);
+
+//}
+//chotafunction(Array);
+
+function chotafunction(string) {
+    console.log("Robert Downey Jr Is Iron Man");
+
+}
+chotafunction();
+
+
+
+
